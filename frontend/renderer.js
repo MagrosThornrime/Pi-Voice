5f79fba4fa520343d79b9f241e6f63deb525ff72
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-05-24 17:26:14 +0200

    Update renderer.js

--- frontend/renderer.js
+++ frontend/renderer.js
@@ -1,18 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const { exit } = require('process');
+
+function writeLog(message) {
+    const logFilePath = path.join(__dirname, 'log.txt');
+    const timestamp = new Date().toISOString();
+    const logMessage = `${timestamp} - ${message}\n`;
+    
+    fs.appendFileSync(logFilePath, logMessage, (err) => {
+        if (err) {
+            console.error("Failed to write to log file:", err);
+        }
+    });
+}
+
 let addon;
 try {
     addon = require('./lib/MIDIReader');
 } 
 catch (e) {
-    console.error("Failed to load the MIDIReader module. Ensure it is built correctly.");
-    console.error(e);
-    process.exit(1);
+    writeLog("Failed to load the MIDIReader module. Ensure it is built correctly.");
+    writeLog(e);
+    exit(1);
 }
 
 const getMIDIMessages = () => {
     try{
-        console.log("Retrieving MIDI messages...");
+        writeLog("Retrieving MIDI messages...");
         const messages = addon.getMIDI();
-        console.log("MIDI messages retrieved:", messages);
+        writeLog(`MIDI messages retrieved: ${messages}`);
         const container = document.getElementById('toPrint');
         messages.forEach(msg => {
             const newP = document.createElement('p');
@@ -23,7 +39,7 @@ const getMIDIMessages = () => {
             }
         });
     } catch (error) {
-        console.error("Error retrieving MIDI messages:", error);
+        writeLog(`Error retrieving MIDI messages: ${error}`);
         const container = document.getElementById('toPrint');
         const newP = document.createElement('p');
         newP.textContent = "Error retrieving MIDI messages: " + error.message;
