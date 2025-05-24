ac24208acaaad9e71ff7734d46bab6116fa0d8a4
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-05-24 16:37:55 +0200

    Update renderer.js

--- frontend/renderer.js
+++ frontend/renderer.js
@@ -1,21 +1,37 @@
-const addon = require('./lib/MIDIReader');
-
-const getMIDIMessages = () => {
-    console.log("Fetching MIDI messages...");
-    const messages = addon.getMIDI();
-    console.log("got messages");
-    const container = document.getElementById('toPrint');
-    messages.forEach(msg => {
-        const newP = document.createElement('p');
-        newP.textContent = msg;
-        container.appendChild(newP);
-        if (msg == "<NO-DEVICE>") {
-            clearInterval(midiGetterID);
-        }
-    });
+let addon;
+try {
+    addon = require('./lib/MIDIReader');
+} 
+catch (e) {
+    console.error("Failed to load the MIDIReader module. Ensure it is built correctly.");
+    console.error(e);
+    process.exit(1);
 }
 
-const midiGetterID = setInterval(getMIDIMessages, 100);
+const getMIDIMessages = () => {
+    try{
+        console.log("Retrieving MIDI messages...");
+        const messages = addon.getMIDI();
+        console.log("MIDI messages retrieved:", messages);
+        const container = document.getElementById('toPrint');
+        messages.forEach(msg => {
+            const newP = document.createElement('p');
+            newP.textContent = msg;
+            container.appendChild(newP);
+            if (msg == "<NO-DEVICE>") {
+                clearInterval(midiGetterID);
+            }
+        });
+    } catch (error) {
+        console.error("Error retrieving MIDI messages:", error);
+        const container = document.getElementById('toPrint');
+        const newP = document.createElement('p');
+        newP.textContent = "Error retrieving MIDI messages: " + error.message;
+        container.appendChild(newP);
+    }
+};
+
+const midiGetterID = setInterval(getMIDIMessages, 1000);
 
 function cleanup() {
     midiModule.cleanup();
