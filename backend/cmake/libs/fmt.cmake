a35ae03364b08a16d9d65a2a95014b750f6f5c22
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-06-12 19:10:20 +0200

    made fmt a shared library

--- backend/cmake/libs/fmt.cmake
+++ backend/cmake/libs/fmt.cmake
@@ -2,8 +2,15 @@ include_guard()
 
 include("cmake/CPM.cmake")
 
-set(FMT_POSITION_INDEPENDENT_CODE ON CACHE BOOL "" FORCE)
-CPMAddPackage("gh:fmtlib/fmt#11.2.0")
+CPMAddPackage(
+    NAME fmt
+    GITHUB_REPOSITORY fmtlib/fmt
+    GIT_TAG 11.2.0
+    OPTIONS
+        "FMT_SHARED=ON"
+        "FMT_DOC=OFF"
+        "FMT_TEST=OFF"
+)
 
 if(fmt_ADDED)
     find_package(fmt)
