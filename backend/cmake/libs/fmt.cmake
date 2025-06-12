6ff6ef2d94dff44a4d03d13a2c14d95ec668e261
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-06-12 19:19:24 +0200

    Update fmt.cmake

--- backend/cmake/libs/fmt.cmake
+++ backend/cmake/libs/fmt.cmake
@@ -2,15 +2,14 @@ include_guard()
 
 include("cmake/CPM.cmake")
 
-CPMAddPackage(
-    NAME fmt
-    GITHUB_REPOSITORY fmtlib/fmt
-    GIT_TAG 11.2.0
-    OPTIONS
-        "FMT_SHARED=ON"
-        "FMT_DOC=OFF"
-        "FMT_TEST=OFF"
-)
+get_property(orig_pic GLOBAL PROPERTY POSITION_INDEPENDENT_CODE)
+set(CMAKE_POSITION_INDEPENDENT_CODE ON CACHE BOOL "Enable PIC for fmt" FORCE)
+CPMAddPackage("gh:fmtlib/fmt#11.2.0")
+if(orig_pic)
+  set_property(GLOBAL PROPERTY POSITION_INDEPENDENT_CODE ${orig_pic})
+else()
+  unset(CMAKE_POSITION_INDEPENDENT_CODE CACHE)
+endif()
 
 if(fmt_ADDED)
     find_package(fmt)
