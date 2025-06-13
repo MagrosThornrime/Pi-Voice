6f8a4411ec541090c8cd9c2f02108f99f7127e4f
MagrosThornrime <magros.thornrime@gmail.com>
2025-06-13 21:31:56 +0200

    Add googletest support

--- backend/cmake/test_exe.cmake
+++ backend/cmake/test_exe.cmake
@@ -3,6 +3,6 @@ include_guard()
 include("cmake/libs/rtmidi.cmake")
 include("cmake/libs/portaudio.cmake")
 
-add_executable(test test.cpp)
+add_executable(miditest miditest.cpp)
 
-target_link_libraries(test rtmidi portaudio fmt::fmt)
+target_link_libraries(miditest rtmidi portaudio fmt::fmt)
---
+++ backend/tests/oscillators.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(Oscillators, Simple) {
+    ASSERT_EQ(1, 1);
+}
