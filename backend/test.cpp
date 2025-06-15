768dc9ca86738756d88d3a4ae684a1e9bb3b0555
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-06-15 19:50:35 +0200

    Update test.cpp

--- backend/test.cpp
+++ backend/test.cpp
@@ -2,18 +2,18 @@
 #include <RtMidi.h>
 
 int main() {
-    RtMidiIn midiIn;
-    std::vector<unsigned char> msg;
-    int n;
-    int i;
-    double stamp;
+	RtMidiIn midiIn;
+	std::vector<unsigned char> msg;
+	int n;
+	int i;
+	double stamp;
 
-    n = midiIn.getPortCount();
-    if (n == 0) {
-        fmt::println("No ports");
-        return 1;
-    }
+	n = midiIn.getPortCount();
+	if (n == 0) {
+		fmt::println("No ports");
+		return 1;
+	}
 
-    midiIn.openPort(0);
-    midiIn.ignoreTypes(false, false, false);
+	midiIn.openPort(0);
+	midiIn.ignoreTypes(false, false, false);
 }
