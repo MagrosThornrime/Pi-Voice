ae44586326d09590ed730c4997d3f1a7af0aab9b
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-06-16 00:04:00 +0200

    Update Data.hpp

--- backend/include/midi/Data.hpp
+++ backend/include/midi/Data.hpp
@@ -3,51 +3,54 @@
 #include <Types.hpp>
 #include "Note.hpp"
 #include <Assert.hpp>
+#include <array>
 
 namespace midi {
 
 /// @brief Data type returned by midi readers
 struct Data {
 
-    /// @brief Status types
-    enum Status : u8 {
-        /// @brief Key was released
-        noteOff = 0x80,
-        /// @brief Key was pressed
-        noteOn = 0x90,
-        
-        // below unsupported
-        polyphonicAftertouch = 0xA0,
-        controlChange = 0xB0,
-        programChange = 0xC0,
-        channelAftertouch = 0xD0,
-        pitchWheel = 0xE0,
-    };
+	/// @brief Status types
+	enum Status: u8 {
+		/// @brief Key was released
+		noteOff = 0x80,
+		/// @brief Key was pressed
+		noteOn = 0x90,
 
-    /// @brief Time since last event
-    double time{};
+		// below unsupported
+		polyphonicAftertouch = 0xA0,
+		controlChange = 0xB0,
+		programChange = 0xC0,
+		channelAftertouch = 0xD0,
+		pitchWheel = 0xE0,
+
+		_offset = noteOff,
+	};
+
+	/// @brief Time since last event
+	double time{};
 	/// @brief Data of event
 	/// @details First element is status + channel, rest are data
-    u8 bytes[3]{};
+	std::array<u8, 3> bytes{};
 
-    /// @brief Checks if Data instance is valid
-    inline bool valid() const noexcept {
-		return bytes[0] || bytes[1] || bytes[2];
-    }
 	/// @brief Checks if Data instance is valid
-    inline operator bool() const noexcept {
-        return valid();
-    }
+	inline bool valid() const noexcept {
+		return bytes[0] || bytes[1] || bytes[2];
+	}
+	/// @brief Checks if Data instance is valid
+	inline operator bool() const noexcept {
+		return valid();
+	}
 
-    /// @brief Returns status from data (4 MSB)
-    constexpr Status status() const noexcept {
-        return Status(bytes[0] & (u8)0xF0);
-    }
-    /// @brief Returns channel from data (4 LSB)
-    constexpr u8 channel() const noexcept {
-        return bytes[0] & (u8)0xF;
-    }
-    
+	/// @brief Returns status from data (4 MSB)
+	constexpr Status status() const noexcept {
+		return Status(bytes[0] & (u8)0xF0);
+	}
+	/// @brief Returns channel from data (4 LSB)
+	constexpr u8 channel() const noexcept {
+		return bytes[0] & (u8)0xF;
+	}
+
 	/// @brief Checks if holds note data
 	inline bool hasNote() const noexcept {
 		return status() == noteOff or status() == noteOn or status() == polyphonicAftertouch;
@@ -57,15 +60,15 @@ struct Data {
 		return status() == noteOff or status() == noteOn;
 	}
 	/// @brief Returns note
-    inline Note note() const {
-        ASSERT(hasNote());
-        return midi::note[bytes[1]];
-    }
-    /// @brief Returns normalized velocity
-    inline f32 velocity() const {
-        ASSERT(hasVelocity());
-        return bytes[2] / 127.f;
-    }
+	inline Note note() const {
+		ASSERT(hasNote());
+		return midi::note[bytes[1]];
+	}
+	/// @brief Returns normalized velocity
+	inline f32 velocity() const {
+		ASSERT(hasVelocity());
+		return bytes[2] / 127.f;
+	}
 };
 
 }
