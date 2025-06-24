463dd16381a738a2397041a2636ed11a064d0a8d
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-06-24 12:46:50 +0200

    Added midi to synth.exe

--- backend/cmake/Synth.cmake
+++ backend/cmake/Synth.cmake
@@ -2,10 +2,12 @@ include_guard()
 
 include("cmake/libs/rtmidi.cmake")
 include("cmake/libs/portaudio.cmake")
+include("cmake/libs/fmt.cmake")
+include("cmake/midi.cmake")
 
 file(GLOB_RECURSE SOURCES src/oscillators/*.cpp)
 
 add_executable(synth Synth.cpp ${SOURCES})
 
 target_include_directories(synth PUBLIC include ${portaudio_SOURCE_DIR}/include)
-target_link_libraries(synth rtmidi portaudio portaudiocpp)
+target_link_libraries(synth rtmidi portaudio portaudiocpp fmt::fmt midi)
