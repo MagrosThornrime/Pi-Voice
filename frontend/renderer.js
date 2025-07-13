1438a392a8a9ea12b83082e9dabbb87a0b9a975f
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-07-13 22:12:20 +0200

    more buttons

--- frontend/renderer.js
+++ frontend/renderer.js
@@ -77,6 +77,90 @@ document.getElementById("call-cpp1").onclick = () => {
         container.appendChild(newP);
 	}
 }
+document.getElementById("call-cpp2").onclick = () => {
+	try{
+		addon.openReader(2);
+		midiGetterID = setInterval(getMIDIMessages, 100);
+	}
+	catch(err) {
+        writeLog(`Error opening port 2: ${err}`);
+        const newP = document.createElement('p');
+        newP.textContent = `Error opening port 2: ${err.message}`;
+        container.appendChild(newP);
+	}
+}
+document.getElementById("call-cpp3").onclick = () => {
+	try{
+		addon.openReader(3);
+		midiGetterID = setInterval(getMIDIMessages, 100);
+	}
+	catch(err) {
+        writeLog(`Error opening port 3: ${err}`);
+        const newP = document.createElement('p');
+        newP.textContent = `Error opening port 3: ${err.message}`;
+        container.appendChild(newP);
+	}
+}
+document.getElementById("call-cpp4").onclick = () => {
+	try{
+		addon.openReader(4);
+		midiGetterID = setInterval(getMIDIMessages, 100);
+	}
+	catch(err) {
+        writeLog(`Error opening port 4: ${err}`);
+        const newP = document.createElement('p');
+        newP.textContent = `Error opening port 4: ${err.message}`;
+        container.appendChild(newP);
+	}
+}
+document.getElementById("call-cpp5").onclick = () => {
+	try{
+		addon.openReader(5);
+		midiGetterID = setInterval(getMIDIMessages, 100);
+	}
+	catch(err) {
+        writeLog(`Error opening port 5: ${err}`);
+        const newP = document.createElement('p');
+        newP.textContent = `Error opening port 5: ${err.message}`;
+        container.appendChild(newP);
+	}
+}
+document.getElementById("call-cpp6").onclick = () => {
+	try{
+		addon.openReader(6);
+		midiGetterID = setInterval(getMIDIMessages, 100);
+	}
+	catch(err) {
+        writeLog(`Error opening port 6: ${err}`);
+        const newP = document.createElement('p');
+        newP.textContent = `Error opening port 6: ${err.message}`;
+        container.appendChild(newP);
+	}
+}
+document.getElementById("call-cpp7").onclick = () => {
+	try{
+		addon.openReader(7);
+		midiGetterID = setInterval(getMIDIMessages, 100);
+	}
+	catch(err) {
+        writeLog(`Error opening port 7: ${err}`);
+        const newP = document.createElement('p');
+        newP.textContent = `Error opening port 7: ${err.message}`;
+        container.appendChild(newP);
+	}
+}
+document.getElementById("call-cpp8").onclick = () => {
+	try{
+		addon.openReader(8);
+		midiGetterID = setInterval(getMIDIMessages, 100);
+	}
+	catch(err) {
+        writeLog(`Error opening port 8: ${err}`);
+        const newP = document.createElement('p');
+        newP.textContent = `Error opening port 8: ${err.message}`;
+        container.appendChild(newP);
+	}
+}
 
 function cleanup() {
     addon.cleanup();
