f90749bb21ac3a6f6df5b235711aa218e6c547cd
Jakub Karczewski <karczewskij@student.agh.edu.pl>
2025-11-21 21:27:18 +0100

    Replaced repeated code with map on json object

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -13,6 +13,7 @@ import {
 import { Chart, useChart } from "@chakra-ui/charts"
 import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from "recharts"
 
+
 export interface Point {
   x: number
   y: number
@@ -58,6 +59,7 @@ function get_adsr_curve(attack: number, decay: number, sustain: number, release:
   return points;
 }
 
+
 function norm(param:number[]){
   return param[0]/100;
 }
@@ -77,48 +79,9 @@ export default function Home() {
   const [sustainValue, setSustainValue] = useState([20])
   const [endSustainValue, setEndSustainValue] = useState([40])
 
-
   const [releaseValue, setReleaseValue] = useState([40])
   const [endReleaseValue, setEndReleaseValue] = useState([40])
 
-
-  // type adsrParams = {
-  //   attackValue: number;
-  //   decayValue: number;
-  //   sustainValue: number;
-  //   releaseValue: number
-  // };
-
-  // type adsrEndParams = {
-  //   endAttackValue: number;
-  //   endDecayValue: number;
-  //   endSustainValue: number;
-  //   endReleaseValue: number
-  // }
-
-  // const [adsrProps, setAdsrProps] = useState<adsrParams>(
-  //   {
-  //     attackValue: 10,
-  //     decayValue: 20,
-  //     sustainValue: 30,
-  //     releaseValue: 40
-  //   }
-  // )
-
-  // const [endAdsrProps, setEndAdsrProps] = useState<adsrEndParams>(
-  //   {
-  //     endAttackValue: 10,
-  //     endDecayValue: 20,
-  //     endSustainValue: 30,
-  //     endReleaseValue: 40
-  //   }
-  // )
-
-  // const data_adsr = useMemo(() => {
-  //   return get_adsr_curve(adsrProps.attackValue/100, adsrProps.decayValue/100, adsrProps.sustainValue/100, adsrProps.releaseValue/100, 1.0, 100);
-  // }, [adsrProps]);
-
-  
   const data_adsr = useMemo(() => {
     return get_adsr_curve(norm(attackValue), norm(decayValue), norm(sustainValue), norm(releaseValue), 1.0, 100);
   }, [attackValue, decayValue, sustainValue, releaseValue]);
@@ -128,9 +91,50 @@ export default function Home() {
     series: [{ name: "y", color: "teal.solid" }]
   })
 
+  const adsrControls = [
+  {
+    label: "Volume",
+    value: volumeValue,
+    setValue: setVolumeValue,
+    setEndValue: setEndVolumeValue,
+    onEnd: (v: number) => window.synthAPI.setAmplitude(v/100),
+  },
+
+  {
+    label: "Attack",
+    value: attackValue,
+    setValue: setAttackValue,
+    setEndValue: setEndAttackValue,
+    onEnd: (v: number) => window.synthAPI.setAttack(10 ** (-v / 10)),
+  },
+
+  {
+    label: "Sustain",
+    value: sustainValue,
+    setValue: setSustainValue,
+    setEndValue: setEndSustainValue,
+    onEnd: (v: number) => window.synthAPI.setSustain(v / 100),
+  },
+
+  {
+    label: "Decay",
+    value: decayValue,
+    setValue: setDecayValue,
+    setEndValue: setEndDecayValue,
+    onEnd: (v: number) => window.synthAPI.setDecay(10 ** (-v / 10))
+  },
+
+  {
+    label: "Release",
+    value: releaseValue,
+    setValue: setReleaseValue,
+    setEndValue: setEndReleaseValue,
+    onEnd: (v: number) => window.synthAPI.setRelease(10 ** (-v / 10))
+  }
+]
+
 
   const charts = [chart_adsr];
-
   return (
     <Box minH="100vh" bg="gray.50" p={10}>
 
@@ -214,143 +218,49 @@ export default function Home() {
       </Grid>
 
       <Box h="10" />
+
       <Grid
         templateColumns={{
           base: "1fr",
           md: "repeat(2, 1fr)",
           lg: "repeat(3, 1fr)",
         }}
+        gap={10} maxW="800px" mx="auto">
 
-        gap={10}
-        maxW="800px"
-        mx="auto"
-      >
-          <Box p={5} bg="grey" rounded="2xl" shadow="md">
-            <Text mb={2} fontWeight="medium" textAlign="center">
-              Volume
-            </Text>
-            <Slider.Root
-              value={volumeValue}
-              onValueChange={(e) => setVolumeValue(e.value)}
-              onValueChangeEnd={(e) => {
-                setEndVolumeValue(e.value);
-                window.synthAPI.setAmplitude(e.value[0] / 100.0);
-              }}
-            >
-              <Slider.Control>
-                <Slider.Track>
-                  <Slider.Range />
-                </Slider.Track>
-                <Slider.Thumbs />
-              </Slider.Control>
-            </Slider.Root>
-            <Stack mt="3" gap="1">
-              <Text>
-                Wartość: <b>{volumeValue}</b>
-              </Text>
-            </Stack>
-          </Box>
-          <Box p={5} bg="grey" rounded="2xl" shadow="md">
-            <Text mb={2} fontWeight="medium" textAlign="center">
-              Attack
-            </Text>
-            <Slider.Root
-              value={attackValue}
-              onValueChange={(e) => setAttackValue(e.value)}
-              onValueChangeEnd={(e) => {
-                setEndAttackValue(e.value);
-                window.synthAPI.setAttack((10**(-e.value[0]/10)));
-              }}
-            >
-              <Slider.Control>
-                <Slider.Track>
-                  <Slider.Range />
-                </Slider.Track>
-                <Slider.Thumbs />
-              </Slider.Control>
-            </Slider.Root>
-            <Stack mt="3" gap="1">
-              <Text>
-                Wartość: <b>{attackValue}</b>
-              </Text>
-            </Stack>
-          </Box>
-          <Box p={5} bg="grey" rounded="2xl" shadow="md">
-            <Text mb={2} fontWeight="medium" textAlign="center">
-              Sustain
-            </Text>
-            <Slider.Root
-              value={sustainValue}
-              onValueChange={(e) => setSustainValue(e.value)}
-              onValueChangeEnd={(e) => {
-                setEndSustainValue(e.value);
-                window.synthAPI.setSustain(e.value[0] / 100.0);
-              }}
-            >
-              <Slider.Control>
-                <Slider.Track>
-                  <Slider.Range />
-                </Slider.Track>
-                <Slider.Thumbs />
-              </Slider.Control>
-            </Slider.Root>
-            <Stack mt="3" gap="1">
-              <Text>
-                Wartość: <b>{sustainValue}</b>
-              </Text>
-            </Stack>
-          </Box>
-          <Box p={5} bg="grey" rounded="2xl" shadow="md">
-            <Text mb={2} fontWeight="medium" textAlign="center">
-              Decay
-            </Text>
-            <Slider.Root
-              value={decayValue}
-              onValueChange={(e) => setDecayValue(e.value)}
-              onValueChangeEnd={(e) => {
-                setEndDecayValue(e.value);
-                window.synthAPI.setDecay(10**(-e.value[0]/10));
-              }
-            }
-            >
-              <Slider.Control>
-                <Slider.Track>
-                  <Slider.Range />
-                </Slider.Track>
-                <Slider.Thumbs />
-              </Slider.Control>
-            </Slider.Root>
-            <Stack mt="3" gap="1">
-              <Text>
-                Wartość: <b>{decayValue}</b>
-              </Text>
-            </Stack>
-          </Box>
-          <Box p={5} bg="grey" rounded="2xl" shadow="md">
-            <Text mb={2} fontWeight="medium" textAlign="center">
-              Release
-            </Text>
-            <Slider.Root
-              value={releaseValue}
-              onValueChange={(e) => setReleaseValue(e.value)}
-              onValueChangeEnd={(e) => {
-                setEndReleaseValue(e.value)
-                window.synthAPI.setRelease(10**(-e.value[0]/10));
-              }}
-            >
-              <Slider.Control>
-                <Slider.Track>
-                  <Slider.Range />
-                </Slider.Track>
-                <Slider.Thumbs />
-              </Slider.Control>
-            </Slider.Root>
-            <Stack mt="3" gap="1">
-              <Text>
-                Wartość: <b>{releaseValue}</b>
-              </Text>
-            </Stack>
-          </Box>
+          {
+            adsrControls.map((ctrl) => (
+              <Box key = {ctrl.label} p={5} bg="grey" rounded="2xl" shadow="md">
+                <Text mb={2} fontWeight="medium" textAlign="center">
+                  {ctrl.label}
+                </Text>
+
+                <Slider.Root
+                  value={ctrl.value}
+                  onValueChange={(e) => ctrl.setValue(e.value)}
+                  onValueChangeEnd={(e) => {
+                    ctrl.setEndValue(e.value);
+                    ctrl.onEnd(e.value[0])
+                  }} >
+
+                  <Slider.Control>
+                    <Slider.Track>
+                      <Slider.Range />
+                    </Slider.Track>
+                    <Slider.Thumbs />
+                  </Slider.Control>
+                </Slider.Root>
+
+                <Stack mt="3" gap="1">
+                  <Text>
+                    Wartość: <b>{ctrl.value}</b>
+                  </Text>
+                </Stack>
+
+              </Box>
+              ) 
+            )
+          }
+
       </Grid>
 
     </Box>
