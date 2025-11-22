097330a28635d2e32ad4eee6856efc45b5633595
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-11-22 18:20:09 +0100

    added getPipeline to Synthesizer

--- backend/include/application/Synthesiser.hpp
+++ backend/include/application/Synthesiser.hpp
@@ -11,97 +11,98 @@ namespace application {
 
 /// @brief Controls audio playback and allows for customizing the played sound
 class Synthesiser {
-    /// @brief Stores all layers that affect the sound (source, effects/filters, etc)
-    std::shared_ptr<pipeline::Pipeline> _pipeline;
+	/// @brief Stores all layers that affect the sound (source, effects/filters, etc)
+	std::shared_ptr<pipeline::Pipeline> _pipeline;
 
-    /// @brief Saves the output to an audio file
-    std::shared_ptr<fileio::FileRecorder> _recorder;
+	/// @brief Saves the output to an audio file
+	std::shared_ptr<fileio::FileRecorder> _recorder;
 
-    /// @brief Audio stream used by port audio
-    std::unique_ptr<portaudio::InterfaceCallbackStream> _stream;
+	/// @brief Audio stream used by port audio
+	std::unique_ptr<portaudio::InterfaceCallbackStream> _stream;
 
-    /// @brief Creates basic sounds (that are later modified in Pipeline) using data from the keyboard
-    std::shared_ptr<polyphonic::VoiceManager> _voiceManager;
+	/// @brief Creates basic sounds (that are later modified in Pipeline) using data from the keyboard
+	std::shared_ptr<polyphonic::VoiceManager> _voiceManager;
 
-    /// @brief Port audio configuration data
-    std::unique_ptr<portaudio::AutoSystem> _autoSys;
+	/// @brief Port audio configuration data
+	std::unique_ptr<portaudio::AutoSystem> _autoSys;
 
-    /// @brief Synchronizes starting/stopping the synthesiser with updating audio
-    std::mutex _mutex;
+	/// @brief Synchronizes starting/stopping the synthesiser with updating audio
+	std::mutex _mutex;
 
-    /// @brief Sample rate of played sound
-    i32 _sampleRate;
+	/// @brief Sample rate of played sound
+	i32 _sampleRate;
 
-    /// @brief Number of output channels played sound
-    i32 _channels;
+	/// @brief Number of output channels played sound
+	i32 _channels;
 
-    /// @brief Tells if the synthesiser is playing the sound
-    bool _running = false;
+	/// @brief Tells if the synthesiser is playing the sound
+	bool _running = false;
 
 public:
-    /// @brief Getter of the _running variable
-    bool isRunning();
+	/// @brief Getter of the _running variable
+	bool isRunning();
 
-    /// @brief Constructor
-    /// @param recordingPath name of the audio file where you want to record the output
-    /// @param channels number of audio output channels
-    /// @param sampleRate sound's sample rate
-    Synthesiser(const std::string& recordingPath, i32 channels, i32 sampleRate);
+	/// @brief Constructor
+	/// @param recordingPath name of the audio file where you want to record the output
+	/// @param channels number of audio output channels
+	/// @param sampleRate sound's sample rate
+	Synthesiser(const std::string& recordingPath, i32 channels, i32 sampleRate);
 
-    /// @brief Tells the voice manager that you pressed a key on the keyboard
-    /// @param key id of the key
-    void pressKey(i32 key);
+	/// @brief Tells the voice manager that you pressed a key on the keyboard
+	/// @param key id of the key
+	void pressKey(i32 key);
 
-    /// @brief Tells the voice manager that you released a key on the keyboard
-    /// @param key id of the key
-    void releaseKey(i32 key);
+	/// @brief Tells the voice manager that you released a key on the keyboard
+	/// @param key id of the key
+	void releaseKey(i32 key);
 
-    /// @brief Starts playing the sound
-    void start();
+	/// @brief Starts playing the sound
+	void start();
 
-    /// @brief Stops playing the sound
-    void stop();
+	/// @brief Stops playing the sound
+	void stop();
 
-    /// @brief Sets global amplitude
-    /// @param amplitude volume of the sound (between 0 and 1)
-    void setAmplitude(f32 amplitude);
+	/// @brief Sets global amplitude
+	/// @param amplitude volume of the sound (between 0 and 1)
+	void setAmplitude(f32 amplitude);
 
-    /// @brief Sets the type of a particular oscillator
-    /// @param type type of the oscillator (see Oscillators.hpp)
-    /// @param index id of the oscillator (0, 1 or 2)
-    void setOscillatorType(oscillators::OscillatorType type, i32 index);
+	/// @brief Sets the type of a particular oscillator
+	/// @param type type of the oscillator (see Oscillators.hpp)
+	/// @param index id of the oscillator (0, 1 or 2)
+	void setOscillatorType(oscillators::OscillatorType type, i32 index);
 
-    /// @brief Sets the amplitude of a particular oscillator (its importance in output)
-    /// @param amplitude amplitude of the oscillator (between 0 and 1)
-    /// @param index id of the oscillator (0, 1 or 2)
-    void setOscillatorAmplitude(f32 amplitude, i32 index);
+	/// @brief Sets the amplitude of a particular oscillator (its importance in output)
+	/// @param amplitude amplitude of the oscillator (between 0 and 1)
+	/// @param index id of the oscillator (0, 1 or 2)
+	void setOscillatorAmplitude(f32 amplitude, i32 index);
 
-    /// @brief Sets the attack factor in ADSR (how fast the amplitude rises to full after pressing the key)
-    /// @param attack attack factor value (between 0 and 1)
-    void setAttack(f32 attack);
+	/// @brief Sets the attack factor in ADSR (how fast the amplitude rises to full after pressing the key)
+	/// @param attack attack factor value (between 0 and 1)
+	void setAttack(f32 attack);
 
-    /// @brief Sets the decay factor in ADSR (how fast the amplitude declines to sustain after reaching full)
-    /// @param decay decay factor value (between 0 and 1)
-    void setDecay(f32 decay);
+	/// @brief Sets the decay factor in ADSR (how fast the amplitude declines to sustain after reaching full)
+	/// @param decay decay factor value (between 0 and 1)
+	void setDecay(f32 decay);
 
-    /// @brief Sets the sustain amplitude in ADSR (amplitude value when you're still holding the key)
-    /// @param sustain sustain amplitude value (between 0 and 1)
-    void setSustain(f32 sustain);
+	/// @brief Sets the sustain amplitude in ADSR (amplitude value when you're still holding the key)
+	/// @param sustain sustain amplitude value (between 0 and 1)
+	void setSustain(f32 sustain);
 
-    /// @brief Sets the release factor value in ADSR (how fast the amplitude declines to 0 after releasing the key)
-    /// @param release release factor value (between 0 and 1)
-    void setRelease(f32 release);
+	/// @brief Sets the release factor value in ADSR (how fast the amplitude declines to 0 after releasing the key)
+	/// @param release release factor value (between 0 and 1)
+	void setRelease(f32 release);
 
-    /// @brief Starts recording the audio.
-    void startRecording();
+	/// @brief Starts recording the audio.
+	void startRecording();
 
-    /// @brief Stops recording the audio.
-    void stopRecording();
+	/// @brief Stops recording the audio.
+	void stopRecording();
 
 	/// @brief Tells where to put audio recordings
 	/// @param path output directory
 	void setRecordingPath(const std::string& path);
 
+	pipeline::Pipeline& getPipeline();
 };
 
 }
--- backend/src/application/Synthesiser.cpp
+++ backend/src/application/Synthesiser.cpp
@@ -2,120 +2,124 @@
 
 namespace application {
 Synthesiser::Synthesiser(const std::string& recordingPath, i32 channels, i32 sampleRate)
-    : _sampleRate(sampleRate), _channels(channels) {
+	: _sampleRate(sampleRate), _channels(channels) {
 	_pipeline = std::make_shared<pipeline::Pipeline>();
 	_recorder = std::make_shared<fileio::FileRecorder>(sampleRate, channels);
-    _autoSys = std::make_unique<portaudio::AutoSystem>();
-    _voiceManager = std::make_shared<polyphonic::VoiceManager>(128, 44100.0f);
+	_autoSys = std::make_unique<portaudio::AutoSystem>();
+	_voiceManager = std::make_shared<polyphonic::VoiceManager>(128, 44100.0f);
 }
 
-void Synthesiser::start(){
-    auto lock = std::lock_guard(_mutex);
-    if(_running){
-        return;
-    }
-    auto& sys = portaudio::System::instance();
-    auto& outputDevice = sys.defaultOutputDevice();
+void Synthesiser::start() {
+	auto lock = std::lock_guard(_mutex);
+	if (_running) {
+		return;
+	}
+	auto& sys = portaudio::System::instance();
+	auto& outputDevice = sys.defaultOutputDevice();
 
-            portaudio::DirectionSpecificStreamParameters outParams(
-            outputDevice,
-            _channels,
-            portaudio::FLOAT32,
-            true,
-            outputDevice.defaultLowOutputLatency(),
-            nullptr
-    );
+	portaudio::DirectionSpecificStreamParameters outParams(
+		outputDevice,
+		_channels,
+		portaudio::FLOAT32,
+		true,
+		outputDevice.defaultLowOutputLatency(),
+		nullptr
+	);
 
-    portaudio::StreamParameters streamParams(
-        portaudio::DirectionSpecificStreamParameters::null(),
-        outParams,
-        _sampleRate,
-        256,
-        paClipOff
-    );
-    _voiceManager->setOscillatorType(oscillators::square, 0);
+	portaudio::StreamParameters streamParams(
+		portaudio::DirectionSpecificStreamParameters::null(),
+		outParams,
+		_sampleRate,
+		256,
+		paClipOff
+	);
+	_voiceManager->setOscillatorType(oscillators::square, 0);
 
-    auto& pipelineRef = *_pipeline.get();
-    pipelineRef.setSource(_voiceManager).addLayer(_recorder);
-    _stream = std::make_unique<portaudio::InterfaceCallbackStream>(streamParams, pipelineRef);
-    _stream->start();
-    _running = true;
+	auto& pipelineRef = *_pipeline.get();
+	pipelineRef.setSource(_voiceManager).addLayer(_recorder);
+	_stream = std::make_unique<portaudio::InterfaceCallbackStream>(streamParams, pipelineRef);
+	_stream->start();
+	_running = true;
 }
 
 void Synthesiser::stop() {
-    auto lock = std::lock_guard(_mutex);
-    if(!_running){
-        return;
-    }
-    _recorder->stop();
-    _stream->stop();
-    _stream->close();
-    _running = false;
+	auto lock = std::lock_guard(_mutex);
+	if (!_running) {
+		return;
+	}
+	_recorder->stop();
+	_stream->stop();
+	_stream->close();
+	_running = false;
 }
 
-void Synthesiser::pressKey(i32 key){
-    auto lock = std::lock_guard(_mutex);
-    _voiceManager->turnOn(key);
+void Synthesiser::pressKey(i32 key) {
+	auto lock = std::lock_guard(_mutex);
+	_voiceManager->turnOn(key);
 }
 
-void Synthesiser::releaseKey(i32 key){
-    auto lock = std::lock_guard(_mutex);
-    _voiceManager->turnOff(key);
+void Synthesiser::releaseKey(i32 key) {
+	auto lock = std::lock_guard(_mutex);
+	_voiceManager->turnOff(key);
 }
 
-bool Synthesiser::isRunning(){
-    auto lock = std::lock_guard(_mutex);
-    return _running;
+bool Synthesiser::isRunning() {
+	auto lock = std::lock_guard(_mutex);
+	return _running;
 }
 
-void Synthesiser::setAmplitude(f32 amplitude){
-    auto lock = std::lock_guard(_mutex);
-    _voiceManager->setAmplitude(amplitude);
+void Synthesiser::setAmplitude(f32 amplitude) {
+	auto lock = std::lock_guard(_mutex);
+	_voiceManager->setAmplitude(amplitude);
 }
 
-void Synthesiser::setOscillatorType(oscillators::OscillatorType type, i32 index){
-    auto lock = std::lock_guard(_mutex);
-    _voiceManager->setOscillatorType(type, index);
+void Synthesiser::setOscillatorType(oscillators::OscillatorType type, i32 index) {
+	auto lock = std::lock_guard(_mutex);
+	_voiceManager->setOscillatorType(type, index);
 }
 
-void Synthesiser::setOscillatorAmplitude(f32 amplitude, i32 index){
-    auto lock = std::lock_guard(_mutex);
-    _voiceManager->setOscillatorAmplitude(amplitude, index);
+void Synthesiser::setOscillatorAmplitude(f32 amplitude, i32 index) {
+	auto lock = std::lock_guard(_mutex);
+	_voiceManager->setOscillatorAmplitude(amplitude, index);
 }
 
-void Synthesiser::setAttack(f32 attack){
-    auto lock = std::lock_guard(_mutex);
-    _voiceManager->setAttack(attack);
+void Synthesiser::setAttack(f32 attack) {
+	auto lock = std::lock_guard(_mutex);
+	_voiceManager->setAttack(attack);
 }
 
-void Synthesiser::setDecay(f32 decay){
-    auto lock = std::lock_guard(_mutex);
-    _voiceManager->setDecay(decay);
+void Synthesiser::setDecay(f32 decay) {
+	auto lock = std::lock_guard(_mutex);
+	_voiceManager->setDecay(decay);
 }
 
-void Synthesiser::setSustain(f32 sustain){
-    auto lock = std::lock_guard(_mutex);
-    _voiceManager->setSustain(sustain);
+void Synthesiser::setSustain(f32 sustain) {
+	auto lock = std::lock_guard(_mutex);
+	_voiceManager->setSustain(sustain);
 }
 
-void Synthesiser::setRelease(f32 release){
-    auto lock = std::lock_guard(_mutex);
-    _voiceManager->setRelease(release);
+void Synthesiser::setRelease(f32 release) {
+	auto lock = std::lock_guard(_mutex);
+	_voiceManager->setRelease(release);
 }
 
-void Synthesiser::startRecording(){
-    auto lock = std::lock_guard(_mutex);
-    _recorder->start();
+void Synthesiser::startRecording() {
+	auto lock = std::lock_guard(_mutex);
+	_recorder->start();
 }
 
-void Synthesiser::stopRecording(){
-    auto lock = std::lock_guard(_mutex);
-    _recorder->stop();
+void Synthesiser::stopRecording() {
+	auto lock = std::lock_guard(_mutex);
+	_recorder->stop();
 }
 
-void Synthesiser::setRecordingPath(const std::string& path){
+void Synthesiser::setRecordingPath(const std::string& path) {
 	auto lock = std::lock_guard(_mutex);
 	_recorder->setOutputDirectory(path);
+}
+
+pipeline::Pipeline& Synthesiser::getPipeline() {
+	return *_pipeline;
 }
 
 }
