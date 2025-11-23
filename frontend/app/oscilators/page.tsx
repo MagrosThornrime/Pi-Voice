c67b723677cc8c579f1ad5f07175c187739d43c7
Jakub Karczewski <karczewskij@student.agh.edu.pl>
2025-11-23 21:43:22 +0100

    Added Function Chart component for better code, also it fixed the error with all oscillators refreshing after changing one XD

--- frontend/app/oscilators/page.tsx
+++ frontend/app/oscilators/page.tsx
@@ -1,10 +1,8 @@
 "use client";
-
 import { Chart, useChart } from "@chakra-ui/charts";
 import { Box,createListCollection,Flex,Group, Grid, Portal,Select,Stack,Text } from "@chakra-ui/react";
-import { useState, useMemo} from "react";
+import React, { useState, useMemo} from "react";
 import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from "recharts";
-
 
 function get_example_data(n: number, domain: number[], func : (X:number) => number) {
   return Array.from({ length: n }, (_, i) => ({
@@ -88,14 +86,78 @@ const oscillatorsFuncMapping: Record<string, (X:number) => number> = {
   sawtooth: ((x) => sawtooth_func(x, 2.0))
 }
 
+type FunctionChartProps = {
+  func: (x: number) => number;
+  domain?: [number, number];
+  n?: number;
+};
 
-export function get_oscillator_chart(oscType: string){
+
+function FunctionChart({func, domain = [0, 10], n = 1000}: FunctionChartProps){
+
   const chart = useChart({
-      data: get_example_data(1000, [0.0, 10.0], oscillatorsFuncMapping[oscType]),
+      data: get_example_data(n, domain, func),
       series: [{ name: "y", color: "teal.solid" }]
     })
-    return chart;
+
+    return (
+      <Chart.Root width={400} height={300} chart={chart}>
+
+        <LineChart data={chart.data}>
+
+          <CartesianGrid vertical={false} />
+
+          <XAxis dataKey="x"
+            label={{ value: "X", position: "bottom" }}
+            stroke={chart.color("border")}
+            tickFormatter={(value) => `${Math.round(value * 100) / 100}`}
+          />
+
+          <YAxis dataKey="y"
+            label={{ value: "Y", position: "left" }}
+            stroke={chart.color("border")}
+            tickFormatter={(value) => `${Math.round(value * 100) / 100}`}
+          />
+
+          <Tooltip
+            animationDuration={100}
+            cursor={false}
+            content={({ active, payload, label }) => {
+              if (active && payload && payload.length) {
+                const x = Math.round(Number(label) * 100) / 100;
+                const y = Math.round(payload[0].value * 100) / 100;
+
+                return (
+                  <Box bg="white" p={3} rounded="md" shadow="md" borderWidth={1}>
+                    <Text fontSize="sm" color="gray.600">x: {x}</Text>
+                    <Text fontSize="sm" color="gray.600">y: {y}</Text>
+                  </Box>
+                );
+              }
+            }}
+          />
+
+          {
+            chart.series.map((item) => (
+              <Line key={item.name}
+                isAnimationActive={false}
+                dataKey={chart.key(item.name)}
+                stroke={chart.color(item.color)}
+                strokeWidth={2}
+                dot={false}
+              />
+            )
+            )
+          }
+
+        </LineChart>
+
+      </Chart.Root>
+    )
 }
+
+
+const MemoFunctionChart = React.memo(FunctionChart);
 
 
 export default function Page() {
@@ -111,9 +173,6 @@ export default function Page() {
     });
   }
 
-
-  const charts = oscillators.map(o => get_oscillator_chart(o));
-
   return(
     <Box minH="100vh" bg="gray.50" p={10}>
       <Grid templateColumns={{
@@ -125,98 +184,43 @@ export default function Page() {
           maxW="1400px"
           mx="auto"
           alignItems="center"
-          justifyItems="center"
-        >
-
+          justifyItems="center" >
         {
-          charts.map((chart, i) => (
-          <Box key = {i}>
-            <Chart.Root width={400} height={300} chart={chart}>
+          oscillators.map((o, i) => (
+            <Box>
+              <MemoFunctionChart key={i} func = {oscillatorsFuncMapping[o]} />
 
-              <LineChart data={chart.data}>
+              <Select.Root collection={oscilatorTypes} variant={"subtle"}
+                onValueChange={(e) => {
+                  changeOscillators(i, e.value[0]);
+                  window.synthAPI.setOscillatorType(e.value[0], 0);
+                }}>
 
-                <CartesianGrid vertical={false} />
+                <Select.HiddenSelect />
+                <Select.Label color={"black"}>{`Oscilator${i + 1}`}</Select.Label>
 
-                <XAxis dataKey="x"
-                  label={{ value: "X", position: "bottom" }}
-                  stroke={chart.color("border")}
-                  tickFormatter={(value) => `${Math.round(value * 100)/100}`} 
-                />
-
-                <YAxis dataKey="y"
-                  label={{ value: "Y", position: "left" }}
-                  stroke={chart.color("border")}
-                  tickFormatter={(value) => `${Math.round(value * 100)/100}`} 
-                />
-
-                <Tooltip
-                  animationDuration={100}
-                  cursor={false}
-                  content={({ active, payload, label }) => {
-                    if (active && payload && payload.length) 
-                    {
-                      const x = Math.round(Number(label) * 100) / 100;
-                      const y = Math.round(payload[0].value * 100) / 100;
-
-                      return (
-                        <Box bg="white" p={3} rounded="md" shadow="md" borderWidth={1}>
-                          <Text fontSize="sm" color="gray.600">x: {x}</Text>
-                          <Text fontSize="sm" color="gray.600">y: {y}</Text>
-                        </Box>
-                        );
-                    }
-                  }} 
-                />
-
-                {
-                  chart.series.map((item) => (
-                    <Line key={item.name}
-                      isAnimationActive={false}
-                      dataKey={chart.key(item.name)}
-                      stroke={chart.color(item.color)}
-                      strokeWidth={2}
-                      dot={false}
-                    />
-                  )
-                  )
-                }
-
-              </LineChart>
-
-            </Chart.Root>
-
-            <Select.Root collection={oscilatorTypes} variant={"subtle"} onValueChange={(e) => {
-              changeOscillators(i, e.value[0]);
-
-              window.synthAPI.setOscillatorType(e.value[0],0);
-            }}>
-
-              <Select.HiddenSelect />
-              <Select.Label color={"black"}>{`Oscilator${i+1}`}</Select.Label>
-              
-              <Select.Control>
-                <Select.Trigger>
-                  <Select.ValueText color = {"black"} placeholder="Select oscillator" />
-                </Select.Trigger>
-                <Select.IndicatorGroup>
-                  <Select.Indicator />
-                </Select.IndicatorGroup>
-              </Select.Control>
-              <Portal>
-                <Select.Positioner>
-                  <Select.Content>
-                    {oscilatorTypes.items.map((oscilator) => (
-                      <Select.Item color = "black" item={oscilator} key={oscilator.value}>
-                        {oscilator.label}
-                        <Select.ItemIndicator />
-                      </Select.Item>
-                    ))}
-                  </Select.Content>
-                </Select.Positioner>
-              </Portal>
-            </Select.Root>
-
-          </Box>
+                <Select.Control>
+                  <Select.Trigger>
+                    <Select.ValueText color={"black"} placeholder="Select oscillator" />
+                  </Select.Trigger>
+                  <Select.IndicatorGroup>
+                    <Select.Indicator />
+                  </Select.IndicatorGroup>
+                </Select.Control>
+                <Portal>
+                  <Select.Positioner>
+                    <Select.Content>
+                      {oscilatorTypes.items.map((oscilator) => (
+                        <Select.Item color="black" item={oscilator} key={oscilator.value}>
+                          {oscilator.label}
+                          <Select.ItemIndicator />
+                        </Select.Item>
+                      ))}
+                    </Select.Content>
+                  </Select.Positioner>
+                </Portal>
+              </Select.Root>
+            </Box>
           ))
         }  
       </Grid>
