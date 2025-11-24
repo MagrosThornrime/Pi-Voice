38a069fc7bb3769a4282dbe5059cc6b31d12999a
PSielchan <147639332+PSielchan@users.noreply.github.com>
2025-11-24 02:49:32 +0100

    oscilators synced with presets

--- frontend/app/oscilators/page.tsx
+++ frontend/app/oscilators/page.tsx
@@ -2,8 +2,9 @@
 
 import { Chart, useChart } from "@chakra-ui/charts";
 import { Box,createListCollection,Flex,Group, Grid, Portal,Select,Stack,Text } from "@chakra-ui/react";
-import { useState, useMemo} from "react";
+import { useState, useMemo, useEffect} from "react";
 import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from "recharts";
+import { usePreset } from "@/components/ui/presetsProvider";
 
 
 function get_example_data(n: number, domain: number[], func : (X:number) => number) {
@@ -100,10 +101,24 @@ export function get_oscillator_chart(oscType: string){
 
 export default function Page() {
   
-  const [oscillators, setOscillator] = useState(["empty", "empty", "empty"])
+  const {
+    presetNr,
+    oscilator1,
+    setOscillator1,
+    oscilator2,
+    setOscillator2,
+    oscilator3,
+    setOscillator3,
+    savePreset,
+  } = usePreset();
+
+  const [oscillators, setOscillator] = useState([oscilator1, oscilator2, oscilator3])
 
   function changeOscillators(i:number, val:string){
     if (i < 0 || i > 2) throw new Error("index out of range (0..2)");
+    if (i==0){setOscillator1(val)};
+    if (i==1){setOscillator2(val)};
+    if (i==2){setOscillator3(val)};
     setOscillator(prev => {
       const next = [...prev];
       next[i] = val;
@@ -111,6 +126,9 @@ export default function Page() {
     });
   }
 
+  useEffect(() => {
+    setOscillator([oscilator1,oscilator2,oscilator3]);
+  }, [oscilator1, oscilator2, oscilator3]);
 
   const charts = oscillators.map(o => get_oscillator_chart(o));
 
@@ -188,7 +206,7 @@ export default function Page() {
             <Select.Root collection={oscilatorTypes} variant={"subtle"} onValueChange={(e) => {
               changeOscillators(i, e.value[0]);
 
-              window.synthAPI.setOscillatorType(e.value[0],0);
+              window.synthAPI.setOscillatorType(e.value[0],i);
             }}>
 
               <Select.HiddenSelect />
