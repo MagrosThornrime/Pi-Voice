8457502175767ce09abc7f4685ad56b5740e329f
Jakub Karczewski <karczewskij@student.agh.edu.pl>
2025-11-24 18:54:24 +0100

    Made a component CheckboxWithHeading in order to reuse it for effects

--- frontend/app/effects_filters/page.tsx
+++ frontend/app/effects_filters/page.tsx
@@ -2,17 +2,16 @@
 import { Box, Checkbox, Button, Fieldset, CheckboxGroup, Heading, Flex, Grid} from "@chakra-ui/react";
 import React, { useState, useMemo} from "react";
 import { standardSchemaResolver } from "@hookform/resolvers/standard-schema"
-import { useController, SubmitHandler, useForm } from "react-hook-form"
+import { useController, ControllerRenderProps, SubmitHandler, useForm, FieldError } from "react-hook-form"
 import { z } from "zod"
 
-
-const formSchema = z.object({
+const FiltersFormSchema = z.object({
   framework: z.array(z.string()).max(3, {
     message: "You cannot select more than 3 effects.",
   }),
 })
 
-type FormData = z.infer<typeof formSchema>
+type FiltersData = z.infer<typeof FiltersFormSchema>
 
 const items = [
   { label: "AllPass", value: "allpass" },
@@ -25,9 +24,83 @@ const items = [
   { label: "Notch", value: "notch" },
 ]
 
+
+type FormWithHeadingProps = {
+  formItems: { label: string; value: string }[];
+  field: ControllerRenderProps<any, any>;
+  buttonText?: string;
+  error?: FieldError;
+  invalid?: boolean;
+};
+
+
+function CheckboxesWithHeading({
+    formItems,
+    field,
+    buttonText,
+    error,
+    invalid,
+}: FormWithHeadingProps
+){
+    return (
+        <Box>
+            <Heading size="3xl" textAlign="center" mb={10} color="teal.600">
+                Select Filters
+            </Heading>
+
+            <Fieldset.Root invalid={invalid}>
+                <Fieldset.Legend color={"teal.600"}> </Fieldset.Legend>
+                <CheckboxGroup color={"black"}
+                    invalid={invalid}
+                    value={field.value}
+                    onValueChange={field.onChange}
+                    name={field.name}
+                >
+                    <Fieldset.Content>
+                        <Grid templateColumns={{
+                                base: "2fr",
+                                md: "repeat(4, 1fr)",
+                                lg: "repeat(4, 1fr)",
+                            }}
+                            gap={10} maxW="800px" mx="auto">
+
+                            {
+                                items.map((item) => (
+                                    <Checkbox.Root key={item.value} value={item.value}>
+                                        <Checkbox.HiddenInput />
+                                        <Checkbox.Control />
+                                        <Checkbox.Label>{item.label}</Checkbox.Label>
+                                    </Checkbox.Root>
+                                ))
+                            }
+
+                        </Grid>
+
+                    </Fieldset.Content>
+
+                <Box h="5" />
+
+                </CheckboxGroup>
+
+                <Box textAlign="center">
+                    {error && (
+                        <Fieldset.ErrorText>{error.message}</Fieldset.ErrorText>
+                    )}
+                    <Box h="2" />
+                    <Button size="sm" type="submit" alignSelf="flex-start">
+                        {buttonText || "Submit"}
+                    </Button>
+                </Box>
+
+            </Fieldset.Root>
+        </Box>
+    )
+}
+
+
 export default function Home() {
-    const { handleSubmit, control, formState: { errors } } = useForm<FormData>({
-        resolver: standardSchemaResolver(formSchema),
+    const { handleSubmit, control, formState: { errors } } = useForm<FiltersData>({
+        resolver: standardSchemaResolver(FiltersFormSchema),
     })
 
     const framework = useController(
@@ -43,56 +116,16 @@ export default function Home() {
     return (
         <Box minH="100vh" bg="gray.50" p={10}>
             <form onSubmit={handleSubmit((data) => console.log(data))}>
+                <CheckboxesWithHeading field = {framework.field}
+                    formItems = {items}
+                    invalid = {invalid}
+                    error={ Array.isArray(errors.framework) 
+                        ? errors.framework[0] 
+                        : errors.framework 
+                    }
+                    buttonText="HAHAHA">
 
-                <Heading size="3xl" textAlign="center" mb={10} color="teal.600">
-                    Select Filters
-                </Heading>
-
-                <Fieldset.Root invalid={invalid}>
-                    <Fieldset.Legend color={"teal.600"}> </Fieldset.Legend>
-                    <CheckboxGroup color={"black"}
-                        invalid={invalid}
-                        value={framework.field.value}
-                        onValueChange={framework.field.onChange}
-                        name={framework.field.name}
-                    >
-                        <Fieldset.Content>
-                            <Grid templateColumns={{
-                                    base: "2fr",
-                                    md: "repeat(4, 1fr)",
-                                    lg: "repeat(4, 1fr)",
-                                }}
-                                gap={10} maxW="800px" mx="auto">
-
-                                {
-                                    items.map((item) => (
-                                        <Checkbox.Root key={item.value} value={item.value}>
-                                            <Checkbox.HiddenInput />
-                                            <Checkbox.Control />
-                                            <Checkbox.Label>{item.label}</Checkbox.Label>
-                                        </Checkbox.Root>
-                                    ))
-                                }
-
-                            </Grid>
-
-                        </Fieldset.Content>
-
-                    <Box h="5" />
-
-                    </CheckboxGroup>
-
-                    <Box textAlign="center">
-                        {errors.framework && (
-                            <Fieldset.ErrorText>{errors.framework.message}</Fieldset.ErrorText>
-                        )}
-                        <Box h="2" />
-                        <Button size="sm" type="submit" alignSelf="flex-start">
-                            Submit
-                        </Button>
-                    </Box>
-                    
-                </Fieldset.Root>
+                </CheckboxesWithHeading>
             </form>
         </Box>
     )
