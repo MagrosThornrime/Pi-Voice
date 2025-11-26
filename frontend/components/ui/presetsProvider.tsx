fc83ba556d4469275eb8cbdf5d54524875b9a21c
PSielchan <147639332+PSielchan@users.noreply.github.com>
2025-11-26 21:53:27 +0100

    load api integration

--- frontend/components/ui/presetsProvider.tsx
+++ frontend/components/ui/presetsProvider.tsx
@@ -53,13 +53,21 @@ export function PresetProvider({ children }) {
       if (!preset) return;
 
       setAttackValue([preset.attack * 100]);
+      window.synthAPI.setAttack(10 ** (-preset.attack * 10));
       setDecayValue([preset.decay * 100]);
+      window.synthAPI.setDecay(10 ** (-preset.decay * 10));
       setSustainValue([preset.sustain * 100]);
+      window.synthAPI.setSustain(preset.sustain);
       setReleaseValue([preset.release * 100]);
+      window.synthAPI.setRelease(10 ** (-preset.release * 10));
       setVolumeValue([preset.volume * 100]);
+      window.synthAPI.setAmplitude(preset.volume);
       setOscillator1(preset.oscilator1);
+      window.synthAPI.setOscillatorType(preset.oscilator1,0);
       setOscillator2(preset.oscilator2);
+      window.synthAPI.setOscillatorType(preset.oscilator1,1);
       setOscillator3(preset.oscilator3);
+      window.synthAPI.setOscillatorType(preset.oscilator1,2);
     });
   }
 
