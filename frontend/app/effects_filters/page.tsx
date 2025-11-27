020f28afa633f9dc456ebf874caa79f027250f1b
Jakub Karczewski <karczewskij@student.agh.edu.pl>
2025-11-27 13:20:22 +0100

    Added properties for each filter to be set by sliders, also added filtersContext

--- frontend/app/effects_filters/page.tsx
+++ frontend/app/effects_filters/page.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { Box, Checkbox, Button, Fieldset, CheckboxGroup, Heading, Flex, Grid} from "@chakra-ui/react";
-import React, { useState, useMemo} from "react";
+import { Box, Checkbox, Button, Fieldset, Stack, CheckboxGroup, Heading, Flex, Grid, Slider} from "@chakra-ui/react";
+import React, { useContext, ReactNode, createContext, useState} from "react";
 import { standardSchemaResolver } from "@hookform/resolvers/standard-schema"
 import { useController, ControllerRenderProps, SubmitHandler, useForm, FieldError } from "react-hook-form"
 import { z } from "zod"
+
 
 const FiltersFormSchema = z.object({
     filters: z.array(z.string()).max(3, {
@@ -15,33 +16,61 @@ const FiltersFormSchema = z.object({
 })
 
 
-
 type FiltersData = z.infer<typeof FiltersFormSchema>
+
+type FiltersContextType = {
+  data: FiltersData;
+  setData: (value: FiltersData) => void;
+};
+
+const FiltersContext = createContext<FiltersContextType | undefined>(undefined);
+
+
+export function FiltersProvider({ children }: { children: ReactNode }) {
+  const [data, setData] = useState<FiltersData>({
+    filters: [],
+    effects: []
+  });
+
+  return (
+    <FiltersContext.Provider value={{ data, setData }}>
+      {children}
+    </FiltersContext.Provider>
+  );
+}
+
+
+export function useFilters() {
+  const ctx = useContext(FiltersContext);
+  if (!ctx) {
+    throw new Error("useFilters must be used inside FiltersProvider");
+  }
+  return ctx;
+}
 
 
 const items = [
-  { label: "AllPass", value: "allpass" },
-  { label: "BandPass", value: "bandpass" },
-  { label: "ButterWorth", value: "butterworth" },
-  { label: "HighPass", value: "highpass" },
-  { label: "HighShelf", value: "highshelf" },
-  { label: "LowPass", value: "lowpass" },
-  { label: "LowShelf", value: "lowshelf" },
-  { label: "Notch", value: "notch" },
+  { label: "AllPass", value: "allpass", opts: ["a", "aa"] },
+  { label: "BandPass", value: "bandpass" , opts: ["b", "bb"]},
+  { label: "ButterWorth", value: "butterworth", opts: ["c", "cc"] },
+  { label: "HighPass", value: "highpass", opts: ["d", "dd"] },
+  { label: "HighShelf", value: "highshelf" , opts: ["e", "ee"] },
+  { label: "LowPass", value: "lowpass", opts: ["f", "ff"]  },
+  { label: "LowShelf", value: "lowshelf", opts: ["g", "gg"] },
+  { label: "Notch", value: "notch", opts: ["h", "hh"] },
 ]
 
 
 const effects = [
-  { label: "aaa", value: "aaa" },
-  { label: "bbb", value: "bbb" },
-  { label: "ccc", value: "ccc" },
-  { label: "ddd", value: "ddd" },
-  { label: "eee", value: "eee" },
-  { label: "fff", value: "fff" },
-  { label: "ggg", value: "ggg" },
-  { label: "hhh", value: "hhh" }
+  { label: "aaa", value: "aaa", opts: ["a", "aa"] },
+  { label: "bbb", value: "bbb", opts: ["b", "bb"] },
+  { label: "ccc", value: "ccc", opts: ["c", "cc"] },
+  { label: "ddd", value: "ddd", opts: ["d", "dd"] },
+  { label: "eee", value: "eee", opts: ["e", "ee"] },
+  { label: "fff", value: "fff", opts: ["f", "ff"] },
+  { label: "ggg", value: "ggg", opts: ["g", "gg"] },
+  { label: "hhh", value: "hhh", opts: ["h", "hh"] }
 ]
-
 
 
 type FormWithHeadingProps = {
@@ -52,6 +81,32 @@ type FormWithHeadingProps = {
   buttonText?: string;
   error?: FieldError;
   invalid?: boolean;
+};
+
+
+const buildInitialState = () => {
+  const state: Record<string, any> = {};
+
+  items.forEach(item => {
+    item.opts.forEach(opt => {
+      state[`${item.value}.${opt}`] = [10]
+    });
+  });
+
+  return state;
+};
+
+
+const buildInitialEndState = () => {
+  const state: Record<string, any> = {};
+
+  items.forEach(item => {
+    item.opts.forEach(opt => {
+      state[`${item.value}.${opt}_end`] = [30]
+    });
+  });
+
+  return state;
 };
 
 
@@ -106,9 +161,11 @@ function CheckboxesWithHeading({
                 </CheckboxGroup>
 
                 <Box textAlign="center">
-                    {error && (
-                        <Fieldset.ErrorText>{error.message}</Fieldset.ErrorText>
-                    )}
+                    {
+                        error && (
+                            <Fieldset.ErrorText>{error.message}</Fieldset.ErrorText>
+                        )
+                    }
                     <Box h="2" />
 
                     {
@@ -126,7 +183,8 @@ function CheckboxesWithHeading({
 }
 
 
-export default function Home() {
+function Page(){
+
     const { handleSubmit, control, formState: { errors } } = useForm<FiltersData>({
         resolver: standardSchemaResolver(FiltersFormSchema),
         defaultValues: {
@@ -135,41 +193,109 @@ export default function Home() {
         }
     })
 
+
    const filtersField = useController({ control, name: "filters" });
    const effectsField = useController({ control, name: "effects" });
+
+   const { data, setData } = useFilters();
+
+   const [Values, setValues] = useState<Record<string, any>>(buildInitialState);
+   const [EndValues, setEndValues] = useState<Record<string, any>>(buildInitialEndState);
+
+   const setSliderValue = (itemValue: string, opt: string, newValue: number) => {
+        setValues(prev => ({
+        ...prev,
+        [`${itemValue}.${opt}`]: newValue
+        }));
+    };
+
+     const setEndSliderValue = (itemValue: string, opt: string, newValue: number) => {
+        setEndValues(prev => ({
+        ...prev,
+        [`${itemValue}.${opt}`]: newValue
+        }));
+    };
 
     const invalid = !!errors.filters
     const invalid_eff = !!errors.effects
 
-    return (
+
+    return(
         <Box minH="100vh" bg="gray.50" p={10}>
-            <form onSubmit={handleSubmit((data) => console.log(data.filters))}>
-                <CheckboxesWithHeading field = {filtersField.field}
-                    formItems = {items}
-                    invalid = {invalid}
-                    error={ Array.isArray(errors.filters) 
-                        ? errors.filters[0] 
+            <form onSubmit={
+                handleSubmit((data) => {
+                    setData(data);
+                    console.log(data.filters, data.effects)
+                }
+            )
+            } >
+                {/* <Stack direction="row" gap={60} justifyContent="center" alignItems="center" > */}
+                <CheckboxesWithHeading field={filtersField.field}
+                    formItems={items}
+                    invalid={invalid}
+                    error={Array.isArray(errors.filters)
+                        ? errors.filters[0]
                         : errors.filters
                     }
-                    ifButton = {false}
+                    ifButton={false}
                     headerText="Select filters"
                     buttonText="HAHAHA">
 
                 </CheckboxesWithHeading>
 
-                <CheckboxesWithHeading field = {effectsField.field}
-                    formItems = {effects}
-                    invalid = {invalid_eff}
-                    error={ Array.isArray(errors.effects) 
-                        ? errors.effects[0] 
+                <CheckboxesWithHeading field={effectsField.field}
+                    formItems={effects}
+                    invalid={invalid_eff}
+                    error={Array.isArray(errors.effects)
+                        ? errors.effects[0]
                         : errors.effects
                     }
-                    ifButton = {true}
-                     headerText="Select effects"
+                    ifButton={true}
+                    headerText="Select effects"
                     buttonText="HAHAHA">
 
                 </CheckboxesWithHeading>
+                {/* </Stack> */}
             </form>
+            {
+                items.filter(item => data.filters.includes(item.label))
+                    .map(obj => obj.opts.map(
+                        opt => {
+                            const key = `${obj.value}.${opt}`;
+                            const Value = Values[key];
+
+                            return (
+                                <Slider.Root
+                                    key = {key}
+                                    value={Value}
+                                    onValueChange={(details) => setSliderValue(obj.value, opt, details.value[0])}
+                                    onValueChangeEnd={(details) => {
+                                        setEndSliderValue(obj.value, `${opt}_end`, details.value[0]);
+                                        console.log("ustawiam dla ", obj.label);
+                                        // ctrl.onEnd(e.value[0]);
+                                    }} >
+
+                                    <Slider.Control>
+                                        <Slider.Track>
+                                            <Slider.Range />
+                                        </Slider.Track>
+                                        <Slider.Thumbs />
+                                    </Slider.Control>
+                                </Slider.Root>
+                            )
+
+                        }
+                    ) )
+            }
         </Box>
+    )
+}
+
+
+export default function Home() {
+    return (
+        <FiltersProvider>
+           <Page/>
+        </FiltersProvider>
     )
 }
