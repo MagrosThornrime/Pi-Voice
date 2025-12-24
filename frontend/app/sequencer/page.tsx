4aa4543a9956d7b9468a5584ab68bfb36ddf72bc
PSielchan <147639332+PSielchan@users.noreply.github.com>
2025-12-24 02:57:14 +0100

    blank page

---
+++ frontend/app/sequencer/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useState } from "react";
+
+import {
+  Box,
+  Heading,
+  Text,
+  Grid,
+  Slider,
+  Stack,
+  Button,
+  List,
+  ListItem,
+} from "@chakra-ui/react";
+
+export default function Page() {
+
+    return (
+        <Box minH="100vh" bg="gray.50" p={10}>
+            
+        </Box>
+    );
+}
