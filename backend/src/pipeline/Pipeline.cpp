06eca9b68a731df6651312a43bbfe28b746b123e
MagrosThornrime <magros.thornrime@gmail.com>
2025-12-31 15:03:42 +0100

    Fix std::move out of bounds segfault

--- backend/src/pipeline/Pipeline.cpp
+++ backend/src/pipeline/Pipeline.cpp
@@ -57,7 +57,9 @@ void Pipeline::move(const u32 curr, const u32 target) {
 	LayerRef item = std::move(_layers[curr]);
 	_layers.erase(_layers.begin() + curr);
 
-	u32 insertIndex = std::min(target, size);
+	const u32 newSize = _layers.size();
+
+	u32 insertIndex = std::min(target, newSize);
 	_layers.insert(_layers.begin() + insertIndex, std::move(item));
 }
 
