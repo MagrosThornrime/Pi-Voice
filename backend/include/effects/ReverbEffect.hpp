d1d6516f74a1ea2c2170fd9e88e53f4c304ca74c
MagrosThornrime <magros.thornrime@gmail.com>
2026-01-08 00:21:27 +0100

    Write docs for application module and most effects

--- backend/include/effects/ReverbEffect.hpp
+++ backend/include/effects/ReverbEffect.hpp
@@ -5,11 +5,12 @@
 
 namespace effects {
 
+    /// @brief Params used by the reverb effect
     struct ReverbParams {
         enum Value: u32 {
-			bufferFrames,
-            feedback,
-            mix,
+			bufferFrames, // base number of frames that can be stored in each buffer (excluding later modification)
+            feedback, // tells how much of the signal is put into the buffers
+            mix, // tells how much of the signal in buffers is put into the output
             _count,
         };
 
@@ -28,8 +29,10 @@ namespace effects {
 
 #undef PARAM_TYPE
 
+    /// @brief Creates an illusion of echo - sound waves reflecting and returning to the source over time
     class ReverbEffect : public Effect {
 
+        /// @brief Buffer lengths are multiplied by it
         static constexpr std::array<f32, 8> _bufferFramesFactors = {
             1.4983f,
             1.5413f,
@@ -41,26 +44,46 @@ namespace effects {
             1.0867f
         };
 
+        /// @brief delay buffers
         std::array<std::vector<f32>, 8> _buffers{};
+
+        /// @brief indices used by delay buffers
         std::array<u32, 8> _indices{};
+
 		u32 _bufferFrames = 10000;
 
         f32 _feedback = 0.1f;
         f32 _mix = 0.5f;
 
+        /// @brief Changes the frequencies affected by the AllPass filters stages
         const std::array<f32, 4> _allPassCoeffs = { 0.03f, 0.07f, 0.11f, 0.17f };
         std::array<AllPassStage, 4> _allPassStages;
 
     public:
+        /// @brief Set one of params described in ReverbEffect struct
+        /// @param param param index
+        /// @param value param value
         pipeline::Layer& setParam(const u32 param, std::any value) override;
+
+        /// @brief Get one of params described in ReverbParams struct
+        /// @param param index
         std::any getParam(const u32 param) override;
 
+        /// @brief Apply the effect to the signal
+        /// @param inputBuffer input signal
+        /// @param outputBuffer output signal
+        /// @param frames number of frames affected
         void processSound(std::vector<f32>& inputBuffer, std::vector<f32>& outputBuffer, u32 frames) override;
 
+        /// @brief Constructor
+        /// @param channels number of channels
+        /// @param sampleRate sounds's sample rate
         ReverbEffect(const u32 channels, const f32 sampleRate);
 
+        /// @brief Gets the type of the effect
         EffectType::Value getEffectType() override;
 
+        /// @brief Updates internal state depending on params
         void refresh() override;
     };
 
